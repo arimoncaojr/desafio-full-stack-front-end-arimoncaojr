e1a8b715f5de3e15ac818b4c0b093a4414c31514
arimoncaojr <106547012+arimoncaojr@users.noreply.github.com>
2023-03-23 16:59:16 -0300

    fix: remove idContact in export context

--- src/contexts/ModalEditContactContext.tsx
+++ src/contexts/ModalEditContactContext.tsx
@@ -14,7 +14,6 @@ export interface IModalContactEdit {
 
 interface IModalEditContactContext {
   showModalEditContact: boolean;
-  idContact: string;
   fullNameContact: string;
   emailContact: string;
   telephoneContact: string;
